22e3ae040894f04c10187196df6f673253897e3b
Ciaran M <cmorinan@gmail.com>
2018-09-14 17:20:44 +0100

    completed to spec

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,33 @@
 class Artist
 
-  attr_reader :name
+  attr_reader :name, :years_active
+
+  @@all = []
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.years_active
+    all.map(&:years_active).inject(:+) / all.count
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries
+    paintings.map(&:gallery)
+  end
+
+  def cities
+    galleries.map(&:city)
+  end
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,10 +2,32 @@ class Gallery
 
   attr_reader :name, :city
 
-def initialize(name, city)
-  @name = name
-  @city = city
-end
+  @@all = []
 
+  def initialize(name, city)
+    @name = name
+    @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.gallery == self }
+  end
+
+  def artists
+    paintings.map(&:artist)
+  end
+
+  def artists_names
+    artists.map(&:name)
+  end
+
+  def artists_experience
+    artists.map(&:years_active).inject(:+) / artists.count
+  end
 
 end
